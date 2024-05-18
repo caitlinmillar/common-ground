200363bcbe295c9de40d773484a94e5d504535f4
Caitlin Millar <caitlin-millar@outlook.com>
2024-05-18 18:55:01 +0100

    removing unneeded template files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import RootComponent from './RootComponent'
 import { persistor, store } from './store/reducers/store'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { ROUTES } from './constants/Routes'
+import HomePage from './pages/HomePage'
+import NotFoundPage from './pages/NotFoundPage'
 
 const App: React.FC = () => {
     return (
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <RootComponent />
+            <Router>
+            <Routes>
+                <Route path="*" element={<NotFoundPage />} />
+                <Route path={ROUTES.HOMEPAGE_ROUTE} element={<HomePage />} />
+            </Routes>
+        </Router>
             </PersistGate>
         </Provider>
     )
--- src/store/reducers/store.js
+++ src/store/reducers/store.js
@@ -1,8 +1,8 @@
-import { ThunkDispatch, UnknownAction, combineReducers, configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage
 import data from './data'
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const rootReducer = combineReducers({
     data
@@ -27,10 +27,8 @@ export const store = configureStore({
         })
 })
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = ThunkDispatch<RootState, any, UnknownAction>
 
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppDispatch = () => useDispatch()
+export const useAppSelector = useSelector
 
 export const persistor = persistStore(store)
