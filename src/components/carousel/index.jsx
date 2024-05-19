b775c7065ee1b794ea7b4f6746037234a42e8475
Caitlin Millar <caitlin-millar@outlook.com>
2024-05-19 18:34:29 +0100

    carousel components

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,56 +1,33 @@
 import React from 'react';
-import './index.css'
+import image1 from '../../assets/images/424879797_289814807444047_4576395149206982886_n.jpg'
+import styled from 'styled-components';
+
+const Icon = styled.span`
+  background-image: none !important
+`
 
 export default function Carousel() {
-    return (
-        <main>
-        <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
-        <div className="carousel-indicators">
-          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  return (
+    <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+    <div className="carousel-inner">
+        <div className="carousel-item active">
+            <img src={image1} className="d-block w-100" alt="First slide" />
         </div>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            {/* Placeholder component */}
-            <div className="container">
-              <div className="carousel-caption text-start">
-                <h1>Example headline.</h1>
-                <p>Some representative placeholder content for the first slide of the carousel.</p>
-                <p><a className="btn btn-lg btn-primary" href="#">Sign up today</a></p>
-              </div>
-            </div>
-          </div>
-          <div className="carousel-item">
-            {/* Placeholder component */}
-            <div className="container">
-              <div className="carousel-caption">
-                <h1>Another example headline.</h1>
-                <p>Some representative placeholder content for the second slide of the carousel.</p>
-                <p><a className="btn btn-lg btn-primary" href="#">Learn more</a></p>
-              </div>
-            </div>
-          </div>
-          <div className="carousel-item">
-            {/* Placeholder component */}
-            <div className="container">
-              <div className="carousel-caption text-end">
-                <h1>One more for good measure.</h1>
-                <p>Some representative placeholder content for the third slide of this carousel.</p>
-                <p><a className="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
-              </div>
-            </div>
-          </div>
+        <div className="carousel-item">
+            <img src={image1} className="d-block w-100" alt="Second slide" />
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
-      </main>
-    )
-}      
+        <div className="carousel-item">
+            <img src={image1} className="d-block w-100" alt="Third slide" />
+        </div>
+    </div>
+    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <Icon className="carousel-control-prev-icon" aria-hidden="true"></Icon>
+        <span className="visually-hidden">Previous</span>
+    </button>
+    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <Icon className="carousel-control-next-icon" aria-hidden="true"></Icon>
+        <span className="visually-hidden">Next</span>
+    </button>
+</div>
+  )
+}
